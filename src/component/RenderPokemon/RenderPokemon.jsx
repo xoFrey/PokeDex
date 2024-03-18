c877860ba08ad2b61a9c9261fac06df122197f77
xoFrey <xizelacar@gmail.com>
2024-03-18 15:06:52 +0100

    home page almost done needs styling

--- src/component/RenderPokemon/RenderPokemon.jsx
+++ src/component/RenderPokemon/RenderPokemon.jsx
@@ -1,9 +1,43 @@
+import { useState, useEffect } from "react";
 import "./RenderPokemon.css";
 
-const RenderPokemon = () => {
+const RenderPokemon = ({ url }) => {
+  const [test, setTest] = useState();
+
+  useEffect(() => {
+    fetch(`${url}`)
+      .then((res) => res.json())
+      .then((data) => setTest(data))
+      .catch((err) => console.log("Fetch mit Dynamischer URL ", err));
+  }, []);
+
   return (
-    <section>
-      <h2>RenderPokemon</h2>
+    <section className="render-pokemon">
+      {test ? (
+        <div className="poke-card">
+          <img
+            src={
+              test.sprites.other.dream_world.front_default != null
+                ? test.sprites.other.dream_world.front_default
+                : test.sprites.other.home.front_default
+            }
+            alt=""
+          />
+          <div>
+            <p>
+              #
+              {test.id < 10
+                ? "00" + test.id
+                : test.id < 100 && test.id > 9
+                ? "0" + test.id
+                : test.id}
+            </p>
+            <h3>{test.name}</h3>
+          </div>
+        </div>
+      ) : (
+        <p>loading....</p>
+      )}
     </section>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,25 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./Home.css";
 import { PokeData } from "../../component/Context/Context";
+import RenderPokemon from "../../component/RenderPokemon/RenderPokemon";
 const Home = () => {
   const { pokemon, setPokemon } = useContext(PokeData);
+  const [pokemonList, setPokemonList] = useState();
 
-  console.log(pokemon);
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1025")
+      .then((res) => res.json())
+      .then((data) => setPokemonList(data))
+      .catch((err) => console.log("Pokemon List Fetch", err));
+  }, []);
+
   return (
     <section className="home">
-      <h2>{pokemon.name}</h2>
+      {pokemonList?.results.map((item, index) => (
+        <div>
+          <RenderPokemon url={item.url} />
+        </div>
+      ))}
     </section>
   );
 };
