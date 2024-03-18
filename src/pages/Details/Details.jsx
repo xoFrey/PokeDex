505714fc8f779898891166afe5cdcb0bc4210a0c
Hakan <hkndevit@gmail.com>
2024-03-18 13:26:31 +0300

    update details

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -2,7 +2,21 @@ import "./Details.css";
 const Details = () => {
   return (
     <section>
-      <h2>Details</h2>
+      <div className="container">
+        <div className="inner-container">
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div>
+        <p>#001 Bulbasar</p>
+      </div>
+      <div>
+        <button className="button">POISON</button>
+        <button className="button">GRASS</button>
+      </div>
+      <div>
+        <h3>ATTACKS AND MOVEMENTS</h3>
+      </div>
     </section>
   );
 };
